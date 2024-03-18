f6a1ba33241b2dc203857d84b0bd9b4a19436aa8
Alec Masterson <alecjmasterson@gmail.com>
2024-03-18 14:43:35 -0500

    update get_teams to include matchups

--- get_teams.py
+++ get_teams.py
@@ -1,34 +1,29 @@
 from typing import List
+import json
 import pandas
 import requests
 
 __CHALLENGE_ID = 240
+__SEED_ORDER = [1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15]
 __YEAR = 2024
+
 
 response: requests.Response = requests.get(f"https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId={__CHALLENGE_ID}&platform=chui&view=chui_default")
 data: List[dict] = response.json()
+data: List[dict] = sorted(data, key=lambda x: (x["scoringPeriodId"], x["scoringPeriodMatchupId"]))
 
-matchups: List[dict] = []
-for matchup in data:
-    matchups.append({
-        "id": matchup["id"],
-        "scoringPeriodId": matchup["scoringPeriodId"],
-        "scoringPeriodMatchupId": matchup["scoringPeriodMatchupId"]
-    })
-
-df: pandas.DataFrame = pandas.DataFrame(data=matchups).sort_values(by=["scoringPeriodId", "scoringPeriodMatchupId"])
-df["bracket_id"] = range(1, len(df) + 1)
-df.to_csv(f"./data/{__YEAR}/matchups.csv", index=False)
+with open(f"./data/{__YEAR}/matchups.json", "w") as file:
+    json.dump(data, file)
+    file.close()
 
 teams: List[dict] = []
 for matchup in data:
     if matchup["scoringPeriodId"] != 1:
         continue
 
-    for team in matchup["possibleOutcomes"]:
+    for team in sorted(matchup["possibleOutcomes"], key=lambda x: x["regionSeed"]):
         teams.append({
-            "bracket_id": ((team["regionId"] - 1) * 16) + team["regionSeed"],
-            "id": team["id"],
+            "bracket_id": ((team["regionId"] - 1) * 16) + __SEED_ORDER.index(team["regionSeed"]) + 1,
             "rank": int([x for x in team["mappings"] if x["type"] == "RANKING"][0]["value"]),
             "record": team["additionalInfo"],
             "region": team["regionId"],
