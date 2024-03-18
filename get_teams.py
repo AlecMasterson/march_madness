6b7893438873546430e1179184f87c43c662195c
Alec Masterson <alecjmasterson@gmail.com>
2024-03-17 21:20:05 -0500

    script for getting teams

---
+++ get_teams.py
@@ -0,0 +1,28 @@
+from typing import List
+import pandas
+import requests
+
+CHALLENGE_ID = 240
+YEAR = 2024
+
+response: requests.Response = requests.get(f"https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId={CHALLENGE_ID}&platform=chui&view=chui_default")
+data: List[dict] = response.json()
+
+teams: List[dict] = []
+for matchup in data:
+    if len(matchup["possibleOutcomes"]) != 2:
+        continue
+
+    for team in matchup["possibleOutcomes"]:
+        teams.append({
+            "id": team["id"],
+            "rank": int([x for x in team["mappings"] if x["type"] == "RANKING"][0]["value"]),
+            "record": team["additionalInfo"],
+            "region": team["regionId"],
+            "seed": team["regionSeed"],
+            "team_abbrev": team["abbrev"],
+            "team_name": team["name"]
+        })
+
+df = pandas.DataFrame(data=teams)
+df.to_csv(f"./data/{YEAR}/teams.csv", index=False)
