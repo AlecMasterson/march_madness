a00e16b1f14f6d2a73c369570da5ec5f36965631
Alec Masterson <alecjmasterson@gmail.com>
2024-03-18 12:34:33 -0500

    get matchup ids

--- get_teams.py
+++ get_teams.py
@@ -2,15 +2,27 @@ from typing import List
 import pandas
 import requests
 
-CHALLENGE_ID = 240
-YEAR = 2024
+__CHALLENGE_ID = 240
+__YEAR = 2024
 
-response: requests.Response = requests.get(f"https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId={CHALLENGE_ID}&platform=chui&view=chui_default")
+response: requests.Response = requests.get(f"https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId={__CHALLENGE_ID}&platform=chui&view=chui_default")
 data: List[dict] = response.json()
+
+matchups: List[dict] = []
+for matchup in data:
+    matchups.append({
+        "id": matchup["id"],
+        "scoringPeriodId": matchup["scoringPeriodId"],
+        "scoringPeriodMatchupId": matchup["scoringPeriodMatchupId"]
+    })
+
+df: pandas.DataFrame = pandas.DataFrame(data=matchups).sort_values(by=["scoringPeriodId", "scoringPeriodMatchupId"])
+df["bracket_id"] = range(1, len(df) + 1)
+df.to_csv(f"./data/{__YEAR}/matchups.csv", index=False)
 
 teams: List[dict] = []
 for matchup in data:
-    if len(matchup["possibleOutcomes"]) != 2:
+    if matchup["scoringPeriodId"] != 1:
         continue
 
     for team in matchup["possibleOutcomes"]:
@@ -25,5 +37,5 @@ for matchup in data:
             "team_name": team["name"]
         })
 
-df = pandas.DataFrame(data=teams).sort_values(by="bracket_id")
-df.to_csv(f"./data/{YEAR}/teams.csv", index=False)
+df: pandas.DataFrame = pandas.DataFrame(data=teams).sort_values(by="bracket_id")
+df.to_csv(f"./data/{__YEAR}/teams.csv", index=False)
